08914d7209eaf63b8d009a3e7af5eb8a9a700566
Thomas Feldmann <mail@tfeldmann.de>
2021-08-10 15:26:33 +0200

    update cli

--- gpsdclient/__main__.py
+++ gpsdclient/__main__.py
@@ -1,4 +1,8 @@
-import argparse
+"""
+Shows human-readable gps output.
+"""
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 from . import GPSDClient
 
@@ -9,7 +13,9 @@ def run(args):
         print(x)
 
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = ArgumentParser(
+    formatter_class=ArgumentDefaultsHelpFormatter, description=__doc__
+)
 parser.add_argument("-H", "--host", default="127.0.0.1", help="The host running gpsd")
 parser.add_argument("-P", "--port", default="2947", help="GPSD port")
 parser.add_argument("-J", "--json", action="store_true", help="Output as JSON strings")
@@ -18,7 +24,7 @@ parser.add_argument(
     "--filter",
     default="TPV",
     help="""
-        Only show specific GPS messages.
+        Only show specific GPS messages in --json mode.
         Pass a comma separated list to show multiple message types (e.g. --filter="TSV,SAX").
         To show all messages, set --filter="".
     """,
