fc4efdd08b580bf51ff669469c7e2dd0c085df17
Thomas Feldmann <mail@tfeldmann.de>
2021-08-12 15:21:21 +0200

    update cli client

--- gpsdclient/__main__.py
+++ gpsdclient/__main__.py
@@ -7,10 +7,50 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from . import GPSDClient
 
 
-def run(args):
-    client = GPSDClient()
-    for x in client.dict_stream():
-        print(x)
+INDENT = "    "
+TPV_FIELDS = "mode time lat lon speed track alt".split()
+
+
+def report_version(data):
+    print("Connected to gpsd v%s" % data["release"])
+
+
+def report_devices(data):
+    print("Devices: ")
+    for device in data["devices"]:
+        print(INDENT + device.get("path", "n/a"))
+
+
+def report_tpv(data):
+    values = [data.get(field, "n/a") for field in TPV_FIELDS]
+    print("\t".join(values))
+
+
+def run(host, port, json, filter):
+    filter_topics = set(filter.upper().replace(" ", "").split(","))
+    client = GPSDClient(host=host, port=port)
+
+    # JSON (raw) output
+    if json:
+        for x in client.json_stream():
+            # TODO: FILTER
+            print(x)
+
+    # human readable output
+    else:
+        needs_tpv_header = True
+        for x in client.dict_stream(convert_datetime=True):
+            if x["class"] == "VERSION":
+                report_version(x)
+                needs_tpv_header = True
+            elif x["class"] == "DEVICES":
+                report_devices(x)
+                needs_tpv_header = True
+            elif x["class"] == "TPV":
+                if needs_tpv_header:
+                    print("\t".join(TPV_FIELDS))
+                    needs_tpv_header = False
+                report_tpv(x)
 
 
 parser = ArgumentParser(
@@ -22,12 +62,11 @@ parser.add_argument("-J", "--json", action="store_true", help="Output as JSON st
 parser.add_argument(
     "-F",
     "--filter",
-    default="TPV",
+    default="",
     help="""
-        Only show specific GPS messages in --json mode.
-        Pass a comma separated list to show multiple message types (e.g. --filter="TSV,SAX").
-        To show all messages, set --filter="".
+        Only show specific GPS messages in --json mode. Pass a comma separated list to
+        show multiple message types (e.g. --filter=TPV,SKY,ATT).
     """,
 )
 args = parser.parse_args()
-print(args)
+run(**vars(args))
